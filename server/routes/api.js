45a2053afe56c63593369083c378e307de72449e
thefonso <thefonso@thefonso.com>
2017-11-07 12:52:26 -0800

    DELETE action is completed CRUD complete now

--- server/routes/api.js
+++ server/routes/api.js
@@ -70,4 +70,18 @@ router.put('/video/:id', function (req, res) {
       }
     });
 });
+
+
+router.delete('/video/:id', function(req, res){
+  console.log('Deleting a video');
+  Video.findByIdAndRemove(req.params.id, function(err, deletedVideo){
+    if(err){
+      res.send("Error deleting video");
+    }else{
+      res.json(deletedVideo);
+    }
+  });
+});
+
+
 module.exports = router;
